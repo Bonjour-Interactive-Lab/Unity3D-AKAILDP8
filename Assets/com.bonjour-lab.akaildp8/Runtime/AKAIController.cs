6ff0b260889e959960491096128377a8fe0e3ea8
Alexandre Rivaux <alex@bonjour-lab.com>
2022-08-15 15:01:16 +0200

    Update package with dynamic naming on Pads/Knobs

--- Assets/com.bonjour-lab.akaildp8/Runtime/AKAIController.cs
+++ Assets/com.bonjour-lab.akaildp8/Runtime/AKAIController.cs
@@ -33,41 +33,38 @@ namespace Bonjour{
         }
 
         protected void InitEvents(){
-            midiin.MidiControls.PAD1.performed += ctx => OnPadInput(ctx, 0);
-            midiin.MidiControls.PAD2.performed += ctx => OnPadInput(ctx, 1);
-            midiin.MidiControls.PAD3.performed += ctx => OnPadInput(ctx, 2);
-            midiin.MidiControls.PAD4.performed += ctx => OnPadInput(ctx, 3);
-            midiin.MidiControls.PAD5.performed += ctx => OnPadInput(ctx, 4);
-            midiin.MidiControls.PAD6.performed += ctx => OnPadInput(ctx, 5);
-            midiin.MidiControls.PAD7.performed += ctx => OnPadInput(ctx, 6);
-            midiin.MidiControls.PAD8.performed += ctx => OnPadInput(ctx, 7);
 
-            midiin.MidiControls.KNOB1.performed += ctx => OnKnobInput(ctx, 0);
-            midiin.MidiControls.KNOB2.performed += ctx => OnKnobInput(ctx, 1);
-            midiin.MidiControls.KNOB3.performed += ctx => OnKnobInput(ctx, 2);
-            midiin.MidiControls.KNOB4.performed += ctx => OnKnobInput(ctx, 3);
-            midiin.MidiControls.KNOB5.performed += ctx => OnKnobInput(ctx, 4);
-            midiin.MidiControls.KNOB6.performed += ctx => OnKnobInput(ctx, 5);
-            midiin.MidiControls.KNOB7.performed += ctx => OnKnobInput(ctx, 6);
-            midiin.MidiControls.KNOB8.performed += ctx => OnKnobInput(ctx, 7);
+            foreach (var action in midiin.asset)
+            {
+                if (action.type == InputActionType.Button)
+                {
+                    action.performed += ctx => OnPadInput(ctx);
+                }
+                else if (action.type == InputActionType.Value)
+                {
+                    action.performed += ctx => OnKnobInput(ctx);
+                }
+            }
         }
 
-        protected void OnPadInput(InputAction.CallbackContext context, int index){
-            if(logInputChange) Debug.Log($"PAD{index} trigger at vel: {context.ReadValueAsObject()}");
+        protected void OnPadInput(InputAction.CallbackContext context){
+            int index = int.Parse((context.action.name[context.action.name.Length - 1]).ToString()) - 1;
+            if (logInputChange) Debug.Log($"PAD{index} trigger at vel: {context.ReadValueAsObject()}");
             AKAIInputChange data = new AKAIInputChange();
             data.type  = TYPE.PAD;
-            data.index = index;
             data.value = (float) context.ReadValueAsObject();
+            data.index = index;
 
             OnAKAIInputChange.Invoke(data);
         }
 
-        protected void OnKnobInput(InputAction.CallbackContext context, int index){
-            if(logInputChange)  Debug.Log($"Knob{index} trigger at value: {context.ReadValueAsObject()}");
+        protected void OnKnobInput(InputAction.CallbackContext context){
+            int index = int.Parse((context.action.name[context.action.name.Length - 1]).ToString()) - 1;
+            if (logInputChange)  Debug.Log($"Knob{index} trigger at value: {context.ReadValueAsObject()}");
             AKAIInputChange data = new AKAIInputChange();
             data.type  = TYPE.KNOB;
-            data.index = index;
             data.value = (float) context.ReadValueAsObject();
+            data.index = index;
 
             OnAKAIInputChange.Invoke(data);
         }
